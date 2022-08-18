593b5e4664325a9adfe58adb9df38cd5fc193ded
Yusuf Yıldız <ysfyldz0077@gmail.com>
2022-08-18 23:58:33 +0300

    create header add create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+        <Header />
+      <h1>Lets build gmail</h1>
+    </div>
+  );
+}
+
+export default App;
