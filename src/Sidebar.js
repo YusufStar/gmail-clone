6b1c9cc8aa4664f3d1ae7c42cbdf47b7ee45ac47
Yusuf Yıldız <ysfyldz0077@gmail.com>
2022-08-19 00:28:48 +0300

    day 1 complate

---
+++ src/Sidebar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Sidebar.css";
+import {Button} from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import InboxIcon from "@material-ui/icons/Inbox";
+import SidebarOption from "./SidebarOption";
+import StarIcon from "@material-ui/icons/Star";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <Button
+                startIcon={<AddIcon fontSize="large" /> }
+                className="sidebar__compose"
+            >
+                Compose
+            </Button>
+
+            <SidebarOption Icon={InboxIcon} title="Inbox" number={54} />
+            <SidebarOption Icon={StarIcon} title="Starred" number={54} />
+        </div>
+    );
+}
+
+export default Sidebar;
